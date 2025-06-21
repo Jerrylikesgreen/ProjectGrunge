9ed13addf1533c37216f7b5a8f912025db0f1460
Jerrylikesgreen <gerardo.azamar11@gmail.com>
2025-06-20 21:44:24 -0400

    Initial Commit.
    Late initial commit. Sadly. Player Controller created. Set up Animation Player and Player State Machine.

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,18 @@
+class_name PlayerControler extends Node2D
+
+@onready var body: MobBody = %MobBody
+
+
+
+
+
+func _physics_process(_delta: float) -> void:
+	# Horizontal axis (–1, 0, +1)
+	var dir := Input.get_axis("Left", "Right")
+	body.set_horizontal_input(dir)
+	
+	
+
+	# Jump request (edge-trigger)
+	if Input.is_action_just_pressed("Up"):
+		body.request_jump()
